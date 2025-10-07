f97d38bd7cd88b62cd3961aac5dcfecf484414ab
Akela-x <mail.akela@googlemail.com>
2025-10-07 19:53:40 +0200

    Add files via upload

--- app/src/main/java/com/example/ps4chart/model/Models.kt
+++ app/src/main/java/com/example/ps4chart/model/Models.kt
@@ -1,1 +1,11 @@
+package com.example.ps4chart.model
 
+data class ExploitRow(
+    val id: String,
+    val firmware: String,
+    val userland: String,
+    val kernel: String,
+    val infoOriginal: String
+)
+
+data class TocItem(val title: String, val anchorId: String)
